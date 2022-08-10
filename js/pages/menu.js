28560593bb1a7d02abec89e97342d51dbf9aefaa
Ludovic NAULOT <pctronique@free.fr>
2022-08-10 07:07:47 +0200

    menu

--- js/pages/menu.js
+++ js/pages/menu.js
@@ -2,10 +2,16 @@ function modifierMenu() {
   if ("id" in sessionTab) {
     let insc = document.getElementById("session-insc-admin");
     let conn = document.getElementById("session-conn-deconn");
+    let insc0 = document.getElementById("session-insc-admin-0");
+    let conn0 = document.getElementById("session-conn-deconn-0");
     insc.href = "./admin_game";
     insc.innerHTML = '<i class="bi bi-wallet-fill"></i>admin';
     conn.href = "./?ind=decon";
     conn.innerHTML = '<i class="bi bi-unlock-fill"></i>déconnexion';
+    insc0.href = "./admin_game";
+    insc0.innerHTML = '<i class="bi bi-wallet-fill"></i>admin';
+    conn0.href = "./?ind=decon";
+    conn0.innerHTML = '<i class="bi bi-unlock-fill"></i>déconnexion';
   }
 }
 
